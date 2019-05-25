4fa9bcdfb66c54cde1220c7475248ce521630d01
913262910 <2978172069@qq.com>
2019-05-25 17:15:21 +0800

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ demo.sh
@@ -0,0 +1,15 @@
+if [ -d $1 ]; then
+	echo "目录存在"
+	exit 1
+else
+	mkdir $1
+	cd $1
+	mkdir css js
+	echo "<!DOCTYPE>" > index.html
+	echo "<title>Hello</title>" >> index.html
+	echo "<h1>Hi</h1>" >> index.html
+	echo "h1{color: red;}" > css/style.css
+	echo "var string = 'HELLO World'" > js/main.js
+	echo "alert(string)" >> js/main.js
+	exit 0
+	fi
